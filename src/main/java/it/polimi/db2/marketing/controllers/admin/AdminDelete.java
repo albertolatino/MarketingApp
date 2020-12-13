ce1400884d8bbcd14166112a8cefdbd9973d2edc
fulcus <francescofgonzales@gmail.com>
2020-12-13 18:39:45 +0100

    delete questionnaires from history page

--- src/main/java/it/polimi/db2/marketing/controllers/admin/AdminDelete.java
+++ src/main/java/it/polimi/db2/marketing/controllers/admin/AdminDelete.java
@@ -1,26 +1,17 @@
 package it.polimi.db2.marketing.controllers.admin;
 
 import it.polimi.db2.marketing.controllers.ServletBase;
-import it.polimi.db2.marketing.ejb.entities.User;
 import it.polimi.db2.marketing.ejb.services.QuestionnaireService;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import javax.ejb.EJB;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,22 +23,11 @@ public class AdminDelete extends ServletBase {
     @EJB(name = "it.polimi.db2.marketing.services/QuestionnaireService")
     private QuestionnaireService questionnaireService;
 
-
     public AdminDelete() {
         super();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        if (redirectIfNotLogged(request, response)) return;
-        if (redirectIfNotAdmin(request, response)) return;
-
-        renderPage(request, response, "/WEB-INF/AdminDelete.html");
-    }
-
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         if (redirectIfNotLogged(request, response)) return;
@@ -73,20 +53,24 @@ public class AdminDelete extends ServletBase {
         }
 
 
-       // deletionDate = incrementDate(deletionDate,1);
+        // deletionDate = incrementDate(deletionDate,1);
 
-        if(questionnaireService.questionnaireAlreadyExist(deletionDate)){
+        if (questionnaireService.questionnaireAlreadyExist(deletionDate)) {
 
             message = "Questionnaire correctly deleted";
             questionnaireService.deleteQuestionnaire(deletionDate);
 
-        }else{
+        } else {
             message = "Cannot retrieve questionnaire for this date";
         }
 
         Map<String, Object> variables = new HashMap<>();
         variables.put("message", message);
-        renderPage(request, response, "/WEB-INF/AdminHome.html", variables);
+
+        String path = getServletContext().getContextPath() + "/AdminHome";
+        response.sendRedirect(path);
+
+        //renderPage(request, response, "/WEB-INF/AdminHome.html", variables);
     }
 
     private boolean isBeforeToday(Date date) {
